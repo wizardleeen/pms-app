4d2da23f4a70294faa0772ed48b7df2bde3d5da1
wizardleeen <wizardleen@gmail.com>
2026-02-23 22:36:06 +0800

    Update Supabase API key

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = 'https://gebnmveredocgpelvibi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlYnNtdmVyZWRvY3BlbHZpYmkiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMzU5OTIwMCwiZXhwIjoxOTM5MTc1MjAwfQ.4kVpGZG6yaXHpljAUfm0g3M0vJkC1p9MzVYfP2VJVg0';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlYm5tdmVyZWRvY2dwZWx2aWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NDE1NjAsImV4cCI6MjA4NzQxNzU2MH0.8KaMWodwLLeQ5Mv1-SkS6k_2M53FpRIAe1WNWxr3hB4';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
